aac4101772e495ef71fa74e0f137e749c8ab56ed
Colin Simon <s81601@htw-dresden.de>
2023-05-12 14:33:08 +0200

    make hfclientmodule importable

--- dsp/modules/__init__.py
+++ dsp/modules/__init__.py
@@ -1,6 +1,7 @@
 from .cache_utils import *
 from .gpt3 import *
 from .hf import HFModel
+from .hf_client import HFModelClient
 from .colbertv2 import ColBERTv2
 from .sentence_vectorizer import *
 from .cohere import *
