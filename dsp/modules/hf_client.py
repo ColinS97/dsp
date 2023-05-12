9be0e63e2dd5dd21fc8213d49e64e92f27dbb50f
Colin Simon <s81601@htw-dresden.de>
2023-05-12 16:56:20 +0200

    adapt hfmodelclient to parse url without port

--- dsp/modules/hf_client.py
+++ dsp/modules/hf_client.py
@@ -4,9 +4,12 @@ from dsp.modules.hf import HFModel
 
 
 class HFModelClient(HFModel):
-    def __init__(self, port, model, url="http://0.0.0.0"):
+    def __init__(self, url, port=None, model="HFRemoteModel"):
         super().__init__(model=model, is_client=True)
-        self.url = f"{url}:{port}"
+        if port is None:
+            self.url = url
+        else:
+            self.url = f"{url}:{port}"
         self.headers = {"Content-Type": "application/json; charset=utf-8"}
 
     def _generate(self, prompt, **kwargs):
