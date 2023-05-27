5fcecc0244763060353ef36aba6075d52bf730da
Colin Simon <s81601@htw-dresden.de>
2023-05-27 15:55:41 +0200

    enable eval to return the df

--- dsp/evaluation/utils.py
+++ dsp/evaluation/utils.py
@@ -87,3 +87,38 @@ def evaluate(fn, dev, metric=EM):
     display(df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'left')]}, {'selector': 'td', 'props': [('text-align', 'left')]}]))
 
     return percentage
+
+def evaluate(fn, dev, metric=EM, return_df=False):
+    data = []
+
+    for example in tqdm.tqdm(dev):
+        question = example.question
+        try:
+            prediction = fn(question)
+        except:
+            #print(f"An error happened processing Question: {question} \n\n")
+            prediction = "Error"
+
+        d = dict(example)
+
+        pred = prediction#.answer
+
+        d['prediction'] = pred
+        if d['prediction'] != "Error":
+            d['correct'] = metric(pred, example.answer)
+        else:
+            d['correct'] = False
+        data.append(d)
+
+    df = pd.DataFrame(data)
+
+    percentage = round(100.0 * df['correct'].sum() / len(dev), 1)
+    print(f"Answered {df['correct'].sum()} / {len(dev)} ({percentage}%) correctly.")
+    df['correct'] = df['correct'].apply(lambda x:'⚠️' if x == 'Error' else '✔️' if x else '❌')
+
+    pd.options.display.max_colwidth = None
+    display(df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'left')]}, {'selector': 'td', 'props': [('text-align', 'left')]}]))
+    if not return_df:
+        return percentage
+    else:
+        return percentage, df
