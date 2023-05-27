0eb5a6118f87cb2a656f7d750f4a82e0595401da
Colin Simon <s81601@htw-dresden.de>
2023-05-27 15:06:42 +0200

    add error handling to eval

--- dsp/evaluation/utils.py
+++ dsp/evaluation/utils.py
@@ -60,21 +60,28 @@ def evaluate(fn, dev, metric=EM):
 
     for example in tqdm.tqdm(dev):
         question = example.question
-        prediction = fn(question)
+        try:
+            prediction = fn(question)
+        except:
+            #print(f"An error happened processing Question: {question} \n\n")
+            prediction = "Error"
 
         d = dict(example)
 
         pred = prediction#.answer
 
         d['prediction'] = pred
-        d['correct'] = metric(pred, example.answer)
+        if d['prediction'] != "Error":
+            d['correct'] = metric(pred, example.answer)
+        else:
+            d['correct'] = False
         data.append(d)
 
     df = pd.DataFrame(data)
 
     percentage = round(100.0 * df['correct'].sum() / len(dev), 1)
     print(f"Answered {df['correct'].sum()} / {len(dev)} ({percentage}%) correctly.")
-    df['correct'] = df['correct'].apply(lambda x: '✔️' if x else '❌')
+    df['correct'] = df['correct'].apply(lambda x:'⚠️' if x == 'Error' else '✔️' if x else '❌')
 
     pd.options.display.max_colwidth = None
     display(df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'left')]}, {'selector': 'td', 'props': [('text-align', 'left')]}]))
